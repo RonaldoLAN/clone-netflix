fdb24855aefba22f89d563866092fcf73851f353
Ronaldo Luiz <ronaldolan2109@gmail.com>
2024-06-21 18:46:28 -0300

    commit-API-TRAILER
    Esse commit adiciona a função do usuário clicar em algum filme da lista e ser levado para uma página que carrega um trailer e mostra algumas informações como nome, uma página para trailers.
    
    Tudo isso utilizando a TMDB API

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  })
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTRlYWViOWI2MDJiZDhlMTM1NDIzYTdlMjdmOTk0NCIsInN1YiI6IjY2NzRkNzdjMDhmMmQ5MTRkY2M1MDcwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.97JxFYyomE0_nToOy7vS7NXa2NvoDsDHYaKeymi1OuY'
+    }
+  };
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  },[])
+
+
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
   )
 }
 
